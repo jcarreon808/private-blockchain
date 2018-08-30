ebfb63b37c1de1749ce40c08315718cd5b45577f
Jan Carreon <Jan.Carreon@NYC-CARREON-J.fios-router.home>
2018-08-30 19:08:09 -0400

    added blockchain id validation request endpoint

--- index.js
+++ index.js
@@ -3,9 +3,33 @@ const app = express();
 const levelSandbox = require('./levelSandbox');
 const simpleChain = require('./simpleChain');
 
-app.use(express.json());      
+app.use(express.json());
 app.use(express.urlencoded());
 
+class BlockchainIDValidation{
+  constructor(){
+    this.requests = {}
+  }
+
+  addRequests(address, timestamp, validationWindow){
+    this.requests[address] = {timeStamp:timestamp, window:validationWindow}
+  }
+}
+
+class ValidationWindow{
+  constructor(){
+    this.timeLeft = 500
+  }
+
+  startCountdown(){
+    if(this.timeLeft !== 0){
+      setInterval(()=>this.timeLeft--, 1000);
+    }
+  }
+
+}
+
+let newReq = new BlockchainIDValidation();
 
 app.get('/block/:blockHeight', (req, res) => {
   levelSandbox.getLevelDBData(req.params.blockHeight).then(block => {
@@ -20,6 +44,25 @@ app.post('/block', (req, res) => {
 
   blockChain.addBlock(newBlock).then(block => res.send(block))
 
+})
+
+app.post('/allow-user-request/', (req, res) => {
+  let timeStamp = new Date().getTime().toString().slice(0,-3);
+  let walletAddress = req.body.address
+
+  let newValidationWindow = new ValidationWindow()
+  newValidationWindow.startCountdown()
+
+  newReq.addRequests(walletAddress, timeStamp, newValidationWindow)
+
+  // setTimeout(()=>{
+  //   console.log(newReq.requests[walletAddress].window.timeLeft)
+  // },1000)
+  res.send({
+    message:`${walletAddress}:${timeStamp}:starRegistry`,
+    requestTimestamp:timeStamp,
+    validationWindow:newValidationWindow.timeLeft
+  })
 })
 
 app.listen(8000, () => console.log('Example app listening on port 8000!'))
